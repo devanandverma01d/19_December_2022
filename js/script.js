b20449af9fb5a4f99816a310b9f279b3a49a7d2f
Devanand Verma <devanandverma01d@gmail.come>
2023-01-08 23:29:54 +0530

    new

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//This is an external JS file Through which we can write JS code
+
+//console.log("Hello from External JS file");
+
+//2. Function calling
+dev();
+
+//1. Function definition
+function dev() {
+    //This is fuction body
+    console.log("Hello Mr. Devanand Verma");
+}
+
